dc52bf863f813a42526fc76bcbae8e3ed5444274
hastyjr <105962575+hastyjr@users.noreply.github.com>
2022-09-04 10:56:21 -0500

    adding in torontoData.json

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,25 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([44.0, -80.0], 2);
+
+// Add GeoJSON data.
+let torontoData = "https://raw.githubusercontent.com/jrhasty/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Grahbbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+
+
+// We create the tile layer that will be the background of our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+// Then we add our 'light' tile layer to the map.
+light.addTo(map);
+});
