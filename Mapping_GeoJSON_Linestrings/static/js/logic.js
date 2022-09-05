55f8f3a3d142da6d30243e201f3b0e077aba787d
hastyjr <105962575+hastyjr@users.noreply.github.com>
2022-09-04 21:09:20 -0500

    update

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -4,28 +4,20 @@ console.log("working");
 // Create the map object with center at the San Francisco airport.
 let map = L.map('mapid').setView([44.0, -80.0], 2);
 
-// Add torontoData GeoJSON data.
-let torontoData = "https://github.com/hastyjr/Mapping_Earthquakes/blob/main/torontoRoutes.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+var torontoData = "https://raw.githubusercontent.com/<hastyjr/Mapping_Earthquakes/main/torontoRoutes.json";
+
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoData).then(function (data) {
   console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  // Create a style for the lines.
-  color: "#ffffa1",
-  weight: 2,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-    + feature.properties.dst + "</h3>");
-  }
-})
-.addTo(map);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
 
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToxken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token={accessToxken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: API_KEY
 });
