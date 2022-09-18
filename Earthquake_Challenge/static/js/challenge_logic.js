085dd0971ee9bb5b7a5b4cdec3abd1643875e503
hastyjr <105962575+hastyjr@users.noreply.github.com>
2022-09-18 18:25:38 -0500

    add 3rd deliverable and rubric

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -15,6 +15,14 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
   accessToken: API_KEY
 });
 
+// Deliverable 3: Add an Additional Map //
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [40.7, -94.5],
@@ -25,10 +33,11 @@ let map = L.map('mapid', {
 // Create a base layer that holds all three maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite": satelliteStreets
+  "Satellite": satelliteStreets,
+  "Dark": dark
 };
 
-// 1. Add a 2nd layer group for the tectonic plate data.
+// 1. Add a 3nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
 let tectonicPlates = new L.LayerGroup();
 let majorEarthquakes = new L.LayerGroup();
@@ -39,7 +48,6 @@ let overlays = {
   "Tectonic Plates": tectonicPlates,
   "Major Earthquakes": majorEarthquakes
 };
-
 
 // Then we add a control to the map that will allow the user to change which
 // layers are visible.
@@ -215,4 +223,53 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
     majorEarthquakes.addTo(map);
 
     // 9. Close the braces and parentheses for the major earthquake data.
-  }});
+  }
+});
+
+
+// Rubric - //
+// 1.dd a third map style as a tile layer object to the challenge_logic.js file.
+// 2. Add the map variable to the base layer object.
+let legend = L.control({
+  position: "bottomright"
+});
+
+legend.onAdd = function () {
+  let div = L.DomUtil.create("div", "info legend");
+
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"
+  ];
+
+  // Loop through intervals to generate a label with a colored square for each interval.
+  for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+  }
+  return div;
+};
+
+// Add legend to the map.
+legend.addTo(map);
+
+// 
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (plateData) {
+  // Adding our geoJSON data, along with style information, to the tectonicplates
+  // layer.
+  L.geoJson(plateData, {
+    color: "#ff6500",
+    weight: 2
+  })
+    .addTo(tectonicPlates);
+
+  // Then add the tectonicplates layer to the map.
+  tectonicPlates.addTo(map);
+});
